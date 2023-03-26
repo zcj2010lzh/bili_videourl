2ba5f8d7fa0648eafb205be487282fe127fa1d8b
zcj2010lzh <1558475353@qq.com>
2023-03-26 12:28:53 +0800

    first commit add index.html and main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+# 1. Try generating with command K. Ask for a pytorch script of a feedforward neural network
+# 2. Then, select the outputted code and hit chat. Ask if there's a bug. Ask how to improve.
+# 3. Try selecting some code and hitting edit. Ask the bot to add residual layers.from flask import Flask
+from flask import Flask, render_template, request
+import youtube_dl
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.after_request
+def cors(environ):
+    environ.headers['Access-Control-Allow-Origin']='*'
+    environ.headers['Access-Control-Allow-Method']='*'
+    environ.headers['Access-Control-Allow-Headers']='x-requested-with,content-type'
+    return environ
+
+@app.route('/getVideoInfo',methods=['GET'])
+def get_video_info():
+    message = request.args.get('videoUrl')
+    # print(message)
+        # 创建 youtube-dl 对象
+    ydl = youtube_dl.YoutubeDL()
+    
+    # 获取视频信息
+    info = ydl.extract_info(message, download=False)
+    
+    # 获取视频的下载链接
+    video_url = info['url']
+    
+    print('视频下载链接：', video_url)
+    # 在这里对数据进行处理
+    return  video_url
+if __name__ == '__main__':
+      app.run(debug=True, port=os.getenv("PORT", default=5000))
